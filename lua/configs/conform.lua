8dcc7ffd529a36fd8bbdf9d7403b99c9a228522a
Chems Eddine Senoussi <chemseddsen@gmail.com>
2024-11-03 23:39:46 +0100

    refactor conform and lsp config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,60 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+
+    formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        vue = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        html = { "prettier" },
+        less = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        yaml = { "prettier" },
+        -- python
+        python = { "ruff_format" },
+        -- php = { "intelephense" },
+        markdown = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        graphql = { "prettier" },
+        handlebars = { "prettier" },
+    },
+    formatters = {
+        python = { "ruff_format" },
+        intelephense = {},
+        ["php-cs-fixer"] = {
+            command = "php-cs-fixer",
+            prepend_args = {
+                "--no-interaction",
+                "--quiet",
+                "--config=.php-cs-fixer.dist.php",
+                "fix",
+                "$FILENAME",
+            },
+            stdin = false,
+        },
+        ["pretty-php"] = {
+            args = {
+                "-t",
+                -- "2",
+                -- "--tab 2",
+                "$FILENAME",
+            },
+        },
+        ["html-lsp"] = {
+            command = "html-lsp",
+        },
+    },
+    notify_on_error = true,
 }
 
 return options
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,28 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        local mason = require "mason"
+
+        local mason_tool_installer = require "mason-tool-installer"
+
+        -- enable mason and configure icons
+        mason.setup {}
+
+        mason_tool_installer.setup {
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "isort", -- python formatter
+                "autopep8", -- python formatter
+                "ruff", -- python formatter
+                "ruff-lsp", -- python formatter
+                "pylint", -- python linter
+                "flake8", -- python linter
+                "eslint_d", -- js linter
+            },
+        }
+    end,
+}
