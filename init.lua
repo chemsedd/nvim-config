0d89c383fdcad911abe251deece935f48888d4d8
Chems Eddine Senoussi <chemseddsen@gmail.com>
2024-11-08 22:53:49 +0100

    add spectre for search/replace

--- init.lua
+++ init.lua
@@ -41,6 +41,18 @@ vim.o.foldlevel = 199 -- Using ufo provider need a large value, feel free to dec
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+-- Spectre config
+vim.keymap.set("n", "<leader>Rr", '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
+vim.keymap.set("n", "<leader>Rc", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search and replace current word",
+})
+vim.keymap.set("v", "<leader>Rs", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+    desc = "Search and replace current selection",
+})
+vim.keymap.set("n", "<leader>Rf", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file",
+})
+
 require("ufo").setup {
     provider_selector = function(bufnr, filetype, buftype)
         return { "treesitter", "indent" }
