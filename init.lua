bd9aad73bd2a2954e53d26f3c6e6b75763176212
Chems Eddine Senoussi <chemseddsen@gmail.com>
2025-06-28 18:10:06 +0100

    feature: Add cursor position save feature

--- init.lua
+++ init.lua
@@ -86,3 +86,21 @@ if vim.g.neovide then
 
     vim.g.experimental_layer_grouping = true
 end
+
+-- Restore cursor position on file open
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        local line = vim.fn.line "'\""
+        if
+            line > 1
+            and line <= vim.fn.line "$"
+            and vim.bo.filetype ~= "commit"
+            and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+        then
+            vim.cmd 'normal! g`"'
+        end
+    end,
+})
