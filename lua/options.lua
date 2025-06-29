10dc6d11cb2c24dc7d774946f8bc0465abb42cb8
Chems Eddine Senoussi <chemseddsen@gmail.com>
2025-06-29 12:03:08 +0100

    feature: Add telescope ui select plugin and config

--- lua/options.lua
+++ lua/options.lua
@@ -62,3 +62,31 @@ api.nvim_create_autocmd("BufDelete", {
 if vim.g.neovide then
     vim.o.guifont = "Fira Code:h13" -- text below applies for VimScript
 end
+
+-- This is your opts table
+require("telescope").setup {
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+                -- even more opts
+            },
+
+            -- pseudo code / specification for writing custom displays, like the one
+            -- for "codeactions"
+            -- specific_opts = {
+            --   [kind] = {
+            --     make_indexed = function(items) -> indexed_items, width,
+            --     make_displayer = function(widths) -> displayer
+            --     make_display = function(displayer) -> function(e)
+            --     make_ordinal = function(e) -> string
+            --   },
+            --   -- for example to disable the custom builtin "codeactions" display
+            --      do the following
+            --   codeactions = false,
+            -- }
+        },
+    },
+}
+-- To get ui-select loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require("telescope").load_extension "ui-select"
